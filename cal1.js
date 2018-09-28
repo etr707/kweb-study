7a757a68fa1eb7efd65a6f76947e5acaf6e654a3
etr707 <dns02023@korea.ac.kr>
2018-09-28 17:59:37 +0900

    My first commit

---
+++ cal1.js
@@ -0,0 +1,34 @@
+const calc = {};
+
+calc.add = function(a,b) {
+    return a + b;
+}
+
+calc.sub = function(a,b) {
+    return a - b;
+}
+
+calc.mul = function(a,b) {
+    return a * b;
+}
+
+calc.div = function(a,b) {
+    if(b == 0) {
+        return new Error('zero divider error.');
+    }
+    return a / b;
+}
+
+calc.mod = function(a,b){
+    if(a == b){
+        return 0;
+    }
+    return a % b;
+    
+}
+
+calc.getE = function(){
+    return Math.E;
+}
+
+module.exports = calc;
---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const cal1 = require('./cal1');
+const cal2 = require('./cal2');
+const cal3 = require('./cal3');
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`2 + 2 = ${cal1.add(2,2)}\n` +
+          `5 - 3 = ${cal2.sub(5,3)}\n` +
+          `6 * 7 = ${cal3.mul(6,7)}\n` +
+          `20 % 6 = ${cal3.mod(20,6)}`);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
